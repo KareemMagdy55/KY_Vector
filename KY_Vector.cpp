eb46093892324afa2b6f90de5f23879f01f30ba9
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2022-12-14 14:18:05 +0200

    Add class main structure
    1- Add attributes
    2- Add constructors
    3- Add a destructor

--- KY_Vector.cpp
+++ KY_Vector.cpp
@@ -1,4 +1,39 @@
 #include "KY_Vector.h"
+#include <iostream>
+#define Temp template<class T>
+
 using namespace std;
 
+Temp
+KYVector<T>::KYVector(int n) {
+    cap = n ;
+    size = 0;
+    vec = new T[n];
+}
+Temp
+KYVector<T>::KYVector(T * arr, int n ) {
+    size = n ;
+    cap =  n ;
+    vec = new T[n];
 
+    for (int i = 0; i < n; ++i) {
+        vec[i] = arr[i];
+    }
+}
+Temp
+KYVector<T>::KYVector(const KYVector<T> &anotherVec) {
+    size = anotherVec.size;
+    cap = anotherVec.cap;
+
+    vec = new T[cap];
+
+    for (int i = 0; i < size; ++i) {
+        vec[i] = anotherVec.vec[i];
+    }
+}
+Temp
+KYVector<T>::~KYVector() {
+    size = 0;
+    cap = 0 ;
+    vec = nullptr;
+}
--- KY_Vector.h
+++ KY_Vector.h
@@ -1,10 +1,21 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
+template <class T>
 
-class mainVec{
+class KYVector{
 private:
-
-
+    int cap ;
+    int size ;
+    T* vec;
 public:
+    // constructors
+    KYVector(int n = 10);
+    KYVector (T*, int);
+    KYVector (const KYVector&);
+
+    // destructor
+    ~KYVector();
 
 };
